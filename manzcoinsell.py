bac15b415f47126bd2fdbfaac0a866bb35a9ab1d
Tom DiCato <tomdicato@gmail.com>
2021-04-09 21:20:33 -0400

    Added check for none return

--- manzcoinsell.py
+++ manzcoinsell.py
@@ -8,30 +8,33 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
 def check_for_sales(api, delay, filename):
+    
     logger.info("Retreiving sales")
     
     manz_tranz = get_collection_sales(slug="manzcoin-nftz",event_type="successful",delay=delay)
 
-    manz_tranz_existing = pd.read_csv('manz_tranz_list.csv')
+    if manz_tranz is not None:
+
+        manz_tranz_existing = pd.read_csv('manz_tranz_list.csv')
     
-    for i in range(len(manz_tranz)):
+        for i in range(len(manz_tranz)):
 
-        if manz_tranz['transaction_id'][i] not in manz_tranz_existing['transaction_id'].values:
+            if manz_tranz['transaction_id'][i] not in manz_tranz_existing['transaction_id'].values:
             
-            price = manz_tranz['total_price'][i]
-            seller = manz_tranz['seller_username'][i]
-            buyer = manz_tranz['buyer_username'][i]
+                price = manz_tranz['total_price'][i]
+                seller = manz_tranz['seller_username'][i]
+                buyer = manz_tranz['buyer_username'][i]
 
-            nl = '\n'       
+                nl = '\n'       
 
-            status_string = f"MANZ COIN SOLD!{nl}From: {seller}{nl}To: {buyer}{nl}For: {price}{nl}#MANZCOIN"
-        
-            api.update_with_media(
-                filename = filename,
-                status = status_string
-                )
-                
-            manz_tranz[['transaction_id','timestamp']].head(i+1).to_csv('manz_tranz_list.csv', mode='a', index=False, header=False)
+                status_string = f"MANZ COIN SOLD!{nl}From: {seller}{nl}To: {buyer}{nl}For: {price}{nl}#MANZCOIN"
+
+                api.update_with_media(
+                    filename = filename,
+                    status = status_string
+                    )
+
+                manz_tranz[['transaction_id','timestamp']].head(i+1).to_csv('manz_tranz_list.csv', mode='a', index=False, header=False)
 
 def main():
     api = create_api()
