881afb9d1ced513a73d1abb31930f8c01c3fa4e7
Tom DiCato <tomdicato@gmail.com>
2021-04-21 13:34:40 -0400

    Updated Manzcoinsell

--- manzcoinsell.py
+++ manzcoinsell.py
@@ -5,6 +5,8 @@ from get_collection_sales import get_collection_sales
 from get_collection_bids import get_collection_bids
 from read_github_csv import read_csv_from_github
 import os 
+import pandas as pd
+import numpy as np
 
 gh_access_token=os.environ.get('MANZCOIN_GITHUB_TOKEN')
 
@@ -13,73 +15,73 @@ logger = logging.getLogger()
 
 def check_for_sales(api, delay, filename):
 
-    logger.info("Retreiving sales")
+    logger.info("Retreiving sales")    
     
+    delay=8000
+
     manz_tranz = get_collection_sales(slug="manzcoin-nftz",event_type="successful",delay=delay)
 
-    manz_bidz = get_collection_bids(slug = "manzcoin-nftz", event_type="bid_entered", delay=delay)
-    get_collection_bids(slug="manzcoin-nftz", event_type="bid_entered", delay = 600)    
+    manz_bidz = get_collection_bids(slug = "manzcoin-nftz", event_type="bid_entered", delay=delay)    
 
     if manz_tranz is not None:
 
         manz_tranz_existing = read_csv_from_github(repo_name='ManzcoinBot', git_file = 'manz_tranz_list.csv', repo_owner='tomdicato', branch='main')
 
+        #find transactions that haven't been processed before
+        manz_tranz_filter=manz_tranz[~manz_tranz.transaction_id.isin(manz_tranz_existing.transaction_id.values)].copy()
+        
+        nl='\n'
+
         j=0
 
-        for i in range(len(manz_tranz)):
+        manz_tranz_filter['status_string']=manz_tranz_filter['asset_name'].astype(str)+' SOLD!\n'\
+        'From: '+ manz_tranz_filter['seller_username'].astype(str) + '\n'\
+        'To: ' + manz_tranz_filter['buyer_username'].astype(str)+'\n'\
+        'For: ' + manz_tranz_filter['total_price'].astype(str) + ' ' + manz_tranz_filter['symbol'].astype(str) + '\n'\
+        '#MANZCOIN'
 
-            if manz_tranz['transaction_id'][i] not in manz_tranz_existing['transaction_id'].values:
-            
-                price=manz_tranz['total_price'][i]
-                seller=manz_tranz['seller_username'][i]
-                buyer=manz_tranz['buyer_username'][i]
-                asset_name=manz_tranz['asset_name'][i]
-                symbol=manz_tranz['symbol'][i]            
+        manz_tranz_filter.apply(lambda x: api.update_with_media(filename=filename,status=x.status_string), axis=1)
 
-                nl='\n'     
+            # api.update_with_media(
+            #     filename=filename,
+            #     status=status_string
+            #     )
 
-                status_string=f"{asset_name} SOLD!{nl}From: {seller}{nl}To: {buyer}{nl}For: {price} {symbol}{nl}#MANZCOIN"
-
-                api.update_with_media(
-                    filename=filename,
-                    status=status_string
-                    )
-
-                j+=1
+        j+=1
 
         updatefilefromgithub(gh_access_token, repo_name='ManzcoinBot',
             git_file = 'manz_tranz_list.csv',repo_owner='tomdicato', branch='main', new_tweets=manz_tranz, message=f"updated {j} transactions")
 
-    if manz_bidz is not None:
+    # if manz_bidz is not None:
 
-        manz_tranz_existing = read_csv_from_github(repo_name='ManzcoinBot', git_file = 'manz_tranz_list.csv', repo_owner='tomdicato', branch='main')      
+    #     manz_tranz_existing = read_csv_from_github(repo_name='ManzcoinBot', git_file = 'manz_tranz_list.csv', repo_owner='tomdicato', branch='main')      
     
-        j=0
+    #     j=0
 
-        for i in range(len(manz_bidz)):
+    #     for i in range(len(manz_bidz)):
 
-            if manz_bidz['transaction_id'][i] not in manz_tranz_existing['transaction_id'].values:
+    #         if manz_bidz['transaction_id'][i] not in manz_tranz_existing['transaction_id'].values:
             
-                bid_amount=manz_bidz['bid_amount'][i]
-                bidder=manz_bidz['bidder_username'][i]
-                symbol=manz_bidz['symbol'][i]
-                asset_name=manz_bidz['asset_name'][i]
-                asset_permalink=manz_bidz['permalink'][i]
+    #             bid_amount=manz_bidz['bid_amount'][i]
+    #             bidder=manz_bidz['bidder_username'][i]
+    #             symbol=manz_bidz['symbol'][i]
+    #             asset_name=manz_bidz['asset_name'][i]
+    #             asset_permalink=manz_bidz['permalink'][i]
 
-                nl = '\n'       
+    #             nl = '\n'       
 
-                status_string = (f"MANZCOIN NFTz BID INCREASED!{nl}From: { bidder }{nl}For: "
-                f"{ bid_amount } { symbol }{nl}{asset_permalink}{nl}#MANZCOIN")
+    #             status_string = (f"MANZCOIN NFTz BID INCREASED!{nl}From: { bidder }{nl}For: "
+    #             f"{ bid_amount } { symbol }{nl}{asset_permalink}{nl}#MANZCOIN")
 
-                api.update_with_media(
-                    filename = filename,
-                    status = status_string
-                    )
+    #             api.update_with_media(
+    #                 filename = filename,
+    #                 status = status_string
+    #                 )
                 
-                j+=1
+    #             j+=1
 
-        updatefilefromgithub(gh_access_token, repo_name='ManzcoinBot',
-            git_file = 'manz_tranz_list.csv',repo_owner='tomdicato', branch='main', new_tweets=manz_bidz, message=f"updated {j} bids")
+    #     updatefilefromgithub(gh_access_token, repo_name='ManzcoinBot',
+    #         git_file = 'manz_tranz_list.csv',repo_owner='tomdicato', branch='main', new_tweets=manz_bidz, message=f"updated {j} bids")
 
 def main():
     api = create_api()
